7c6fc7ae2582b97d17ac08b01670094d0f0da04f
ShreyZephico <shrey.shah@zephico.com>
2026-04-22 14:44:29 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/",
+    base: "/",
     server: {
       proxy: {
         // Store backend endpoints
